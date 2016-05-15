cbe144717a9bd5d80de5775f57277bec1ac2ac79
changdongsong <changdongsong@126.com>
2016-05-15 09:25:17 +0800

    完成验证功能是否可以实现，结果为可以

---
+++ EVFile/EVFile.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EVFile
+{
+    public class FileInfo
+    {
+        #region Variables
+        private string path;
+        private string filename;
+        private string extname;
+
+        #endregion
+
+        #region Fields
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+
+            set
+            {
+                path = value;
+            }
+        }
+
+        public string Filename
+        {
+            get
+            {
+                return filename;
+            }
+
+
+        }
+
+        public string Filepath
+        {
+            get
+            {
+                return path + filename;
+            }
+        }
+
+        public string Extname
+        {
+            get
+            {
+                return extname;
+            }
+
+            set
+            {
+                extname = value;
+            }
+        }
+
+        #endregion
+
+        #region constructions
+        public FileInfo(string _path, string _extname)
+        {
+            path = _path;
+            extname = _extname;
+            genFileName();
+        }
+        #endregion
+
+        #region private methods
+        private void genFileName()
+        {
+            filename = Guid.NewGuid() + "." + extname;
+            return;
+        }
+        #endregion
+
+        #region public methods
+        public void WriteText(string _contents)
+        {
+            System.IO.File.WriteAllText(path + filename, _contents);
+        }
+
+        public void MoveFile(string _filePath)
+        {
+            System.IO.File.Copy(_filePath, path + filename, true);
+            System.IO.File.Delete(_filePath);
+        }
+        #endregion
+    }
+}
---
+++ IESnapPhoto/FrmMain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IESnapPhoto
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private void btnSnap_Click(object sender, EventArgs e)
+        {
+            Bitmap bit = EVWB.GetHtmlToImage.GetHtmlImage(new Uri(@"http://10.151.65.113/BPM/YZSoft/Forms/XForm/ExpForms/ExpCenForms/Exp_Cen_GeneralCost2.aspx?app=ExpFormService%2fExpCenFormService%2fExp_Cen_GeneralCost&token=dae18004a26d1c6ef735703503f83edb6f03e02e&tid=6499&key=6499&md=App&formstate=Read"), 1200);
+            EVFile.FileInfo fi = new EVFile.FileInfo("d:\\", "jpg");
+            bit.Save(fi.Filepath, System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wbIE.ScrollBarsEnabled = false;　　//禁用滚动条
+            wbIE.ScriptErrorsSuppressed = false;  //禁用脚本错误
+           
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void wbIE_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.ToString().IndexOf("login") != -1)
+            {
+                wbIE.Document.GetElementById("_txtUserId").SetAttribute("value", "dschang");
+                wbIE.Document.GetElementById("_txtPassword").SetAttribute("value", "Call3275Ca");
+                wbIE.Document.GetElementById("_btnLogin").InvokeMember("click");
+            }
+            else
+            {
+                btnSnap_Click(sender, e);
+            }
+        }
+    }
+}
